39c718a013b5c6cbd3cf001ab2ae73a1e065a1e3
Dario Oroz <dario.oroz@fer.hr>
2017-10-29 16:29:53 +0100

    Zadatak 1

---
+++ Zadatak1/Student.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace Zadatak1
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+
+    public class Student
+    {
+        public Student(string name, string jmbag)
+        {
+            Name = name;
+            Jmbag = jmbag;
+        }
+
+        public string Name { get; set; }
+        public string Jmbag { get; set; }
+        public Gender Gender { get; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Student student &&
+                   Name == student.Name &&
+                   Jmbag == student.Jmbag &&
+                   Gender == student.Gender;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 430495180;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Name);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Jmbag);
+            hashCode = hashCode * -1521134295 + Gender.GetHashCode();
+            return hashCode;
+        }
+
+        public static bool operator ==(Student s1, Student s2)
+        {
+            return s1 != null && s1.Equals(s2);
+        }
+
+        public static bool operator !=(Student s1, Student s2)
+        {
+            return s1 != null && !s1.Equals(s2);
+        }
+    }
+}
