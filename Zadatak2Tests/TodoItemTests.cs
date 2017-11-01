17a685a3a4fcac2740ceb0de754e0e8b39a3a90a
Dario Oroz <dario.oroz@fer.hr>
2017-11-01 18:23:56 +0100

    Zadatak 3

---
+++ Zadatak2Tests/TodoItemTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Zadatak2.Tests
+{
+    [TestClass]
+    public class TodoItemTests
+    {
+        [TestMethod]
+        public void MarkAsCompletedTest()
+        {
+            var t = new TodoItem("test");
+            Assert.AreEqual(t.IsCompleted, false);
+            Assert.AreEqual(t.MarkAsCompleted(), true);
+            Assert.AreEqual(t.IsCompleted, true);
+            Assert.AreEqual(t.MarkAsCompleted(), false);
+        }
+
+        [TestMethod]
+        public void EqualsTest()
+        {
+            var t1 = new TodoItem("test");
+            var t2 = new TodoItem("test");
+            Assert.AreEqual(t1.Equals(t2), false);
+        }
+
+        [TestMethod]
+        public void GetHashCodeTest()
+        {
+            var t1 = new TodoItem("test");
+            var t2 = new TodoItem("test");
+            Assert.AreNotEqual(t1.GetHashCode(), t2.GetHashCode());
+        }
+    }
+}
---
+++ Zadatak2Tests/TodoRepositoryTests.cs
@@ -0,0 +1,157 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Zadatak2.Tests
+{
+    [TestClass]
+    public class TodoRepositoryTests
+    {
+        [TestMethod]
+        public void GetTest()
+        {
+            var repo = new TodoRepository(null);
+            var t1 = new TodoItem("1");
+            Assert.AreEqual(repo.Get(t1.Id), null);
+            repo.Add(t1);
+            Assert.AreEqual(repo.Get(t1.Id), t1);
+            repo.Remove(t1.Id);
+            Assert.AreEqual(repo.Get(t1.Id), null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DuplicateTodoItemException))]
+        public void AddTest_ThrowsDuplicateTodoItemException()
+        {
+            var repo = new TodoRepository(null);
+            var t1 = new TodoItem("1");
+            repo.Add(t1);
+            repo.Add(t1);
+        }
+
+        [TestMethod]
+        public void AddTest()
+        {
+            var repo = new TodoRepository(null);
+            Assert.AreEqual(repo.GetAll().Count, 0);
+            for (var i = 0; i < 10; i++)
+            {
+                var t = new TodoItem("test");
+                repo.Add(t);
+            }
+            Assert.AreEqual(repo.GetAll().Count, 10);
+        }
+
+        [TestMethod]
+        public void RemoveTest()
+        {
+            var repo = new TodoRepository(null);
+            var t1 = new TodoItem("1");
+            var t2 = new TodoItem("2");
+            var t3 = new TodoItem("3");
+            var t4 = new TodoItem("4");
+            var t5 = new TodoItem("5");
+            repo.Add(t1);
+            repo.Add(t2);
+            repo.Add(t3);
+            repo.Add(t4);
+            repo.Add(t5);
+
+            Assert.AreEqual(repo.Remove(t2.Id), true);
+            Assert.AreEqual(repo.GetAll().Count, 4);
+            Assert.AreEqual(repo.Remove(t2.Id), false);
+        }
+
+        [TestMethod]
+        public void UpdateTest()
+        {
+            var repo = new TodoRepository(null);
+            var t1 = new TodoItem("1");
+            Assert.AreEqual(repo.Update(t1), t1);
+            Assert.AreEqual(repo.Update(t1), t1);
+            t1.Text = "2";
+            Assert.AreEqual(repo.Update(t1), t1);
+        }
+
+        [TestMethod]
+        public void MarkAsCompletedTest()
+        {
+            var repo = new TodoRepository(null);
+            var t1 = new TodoItem("1");
+            repo.Add(t1);
+            Assert.AreEqual(repo.MarkAsCompleted(t1.Id), true);
+            Assert.AreEqual(repo.MarkAsCompleted(t1.Id), false);
+            Assert.AreEqual(t1.IsCompleted, true);
+        }
+
+        [TestMethod]
+        public void GetAllTest()
+        {
+            var repo = new TodoRepository(null);
+            var t1 = new TodoItem("1");
+            var t2 = new TodoItem("2");
+            var t3 = new TodoItem("3");
+            var t4 = new TodoItem("4");
+            var t5 = new TodoItem("5");
+            repo.Add(t1);
+            repo.Add(t2);
+            repo.Add(t3);
+            repo.Add(t4);
+            repo.Add(t5);
+            Assert.AreEqual(repo.GetAll().Count, 5);
+        }
+
+        [TestMethod]
+        public void GetActiveTest()
+        {
+            var repo = new TodoRepository(null);
+            var t1 = new TodoItem("1");
+            var t2 = new TodoItem("2");
+            var t3 = new TodoItem("3");
+            var t4 = new TodoItem("4");
+            var t5 = new TodoItem("5");
+            repo.Add(t1);
+            repo.Add(t2);
+            repo.Add(t3);
+            repo.Add(t4);
+            repo.Add(t5);
+            Assert.AreEqual(repo.GetActive().Count, 5);
+            t1.MarkAsCompleted();
+            Assert.AreEqual(repo.GetActive().Count, 4);
+        }
+
+        [TestMethod]
+        public void GetCompletedTest()
+        {
+            var repo = new TodoRepository(null);
+            var t1 = new TodoItem("1");
+            var t2 = new TodoItem("2");
+            var t3 = new TodoItem("3");
+            var t4 = new TodoItem("4");
+            var t5 = new TodoItem("5");
+            repo.Add(t1);
+            repo.Add(t2);
+            repo.Add(t3);
+            repo.Add(t4);
+            repo.Add(t5);
+            Assert.AreEqual(repo.GetCompleted().Count, 0);
+            t1.MarkAsCompleted();
+            Assert.AreEqual(repo.GetCompleted().Count, 1);
+        }
+
+        [TestMethod]
+        public void GetFilteredTest()
+        {
+            var repo = new TodoRepository(null);
+            var t1 = new TodoItem("1");
+            var t2 = new TodoItem("2");
+            var t3 = new TodoItem("3");
+            var t4 = new TodoItem("4");
+            var t5 = new TodoItem("5");
+            repo.Add(t1);
+            repo.Add(t2);
+            repo.Add(t3);
+            repo.Add(t4);
+            repo.Add(t5);
+            Assert.AreEqual(repo.GetFiltered(item => int.Parse(item.Text) > 2).Count, 3);
+        }
+    }
+}
