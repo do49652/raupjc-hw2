ba3469d7595d0855d4aeac6dcde452467d01ad74
Dario Oroz <dario.oroz@fer.hr>
2017-11-03 21:33:28 +0100

    Zadatak 6

---
+++ Zadatak6/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace Zadatak6
+{
+    public class Class1
+    {
+        public static async Task<int> FactorialDigitSum(int n)
+        {
+            var result = 1;
+
+            for (var i = 1; i <= n; i++)
+                result *= i;
+
+            return result;
+        }
+    }
+}
