4b8f746ac334c752fd6e407409be11d2ee338108
Dario Oroz <dario.oroz@fer.hr>
2017-11-03 21:33:37 +0100

    Zadatak 7

---
+++ Zadatak7/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Zadatak7
+{
+    public class Class1
+    {
+        public static void LetsSayUserClickedAButtonOnGuiMethod()
+        {
+            var result = GetTheMagicNumber().Result;
+            Console.WriteLine(result);
+        }
+
+        private static async Task<int> GetTheMagicNumber()
+        {
+            return await IKnowIGuyWhoKnowsAGuyAsync();
+        }
+
+        private static async Task<int> IKnowIGuyWhoKnowsAGuyAsync()
+        {
+            return await IKnowWhoKnowsThisAsync(10) + await IKnowWhoKnowsThisAsync(5);
+        }
+
+        private static async Task<int> IKnowWhoKnowsThisAsync(int n)
+        {
+            return await Zadatak6.Class1.FactorialDigitSum(n);
+        }
+    }
+}
